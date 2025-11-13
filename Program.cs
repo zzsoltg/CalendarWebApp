c5521411eab2a6785ffd928fd4bd09e8ea6537d6
Zimmermann Zsolt Gábor <zimmermannzsg@griffsoft.hu>
2025-11-13 12:57:56 +0100

    Adding Radzen to project

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using CalendarWebApp.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+builder.Services.AddRadzenComponents();
 
 builder.Services.AddAuthentication(options =>
     {
